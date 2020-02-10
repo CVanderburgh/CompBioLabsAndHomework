55f83745726954269ac1d77bac569571ad4ca2ff
CVanderburgh <qiucaihong67@gmail.com>
2020-02-09 18:05:44 -0700

    Modification of the lab01 final problem script

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,3 +1,12 @@
 cut -f 2-4 -d, PredPreyData.csv > PredPreyData_cut.csv
 cat PredPreyData_cut.csv | head -1 && cat PredPreyData_cut.csv | tail -10
 
+
+
+### A better version after Sam's feedback
+cut -f 2-4 -d, PredPreyData.csv > PredPreyData_cut.csv #cut the first column and store it into a temporarily file
+cat PredPreyData_cut.csv | head -1 >SubsetPredPreyData.csv # Create the subsetdata file with just the header
+cat PredPreyData_cut.csv | tail -10 >> SubsetPredPreyData.csv  # Combine the last 10 lines of output into the new Subset file
+rm PredPreyData_cut.csv  #remove the temporarily file, make the folder clean
+
+
